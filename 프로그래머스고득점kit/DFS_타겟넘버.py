2260b1eb0f94b79356dd6dd9adadc7bb5979559c
timotomate <masonmount323@gmail.com>
2022-04-13 22:24:57 +0900

    22:24

---
+++ 프로그래머스고득점kit/DFS_타겟넘버.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+
+    queue.append([numbers[0], 0])
+    queue.append([-1 * numbers[0], 0])
+
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp + numbers[idx], idx])
+            queue.append([temp - numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+
+    return answer
+
+
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
+print(solution(numbers, target))
