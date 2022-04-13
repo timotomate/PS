d9624c409e6183d2412c45c18a6450bba3c81f1c
timotomate <masonmount323@gmail.com>
2022-04-13 17:41:36 +0900

    정렬_HINDEX

--- 프로그래머스고득점kit/정렬_HINDEX.py
+++ 프로그래머스고득점kit/정렬_HINDEX.py
@@ -1,3 +1,11 @@
-a = [1, 2, 3, 4, 5]
-
-print(len(a[2:]))
+def solution(citations):
+    citations = sorted(citations)
+    
+    l = len(citations)
+    # citations = [0, 1, 3, 5, 6]
+    for i in range(l):
+        if citations[i] >= l-i: 
+            # citations[i] = 인용된 횟수(h)
+            # l-i = 인용된 논문의 갯수를 최대값 부터 차례대로 줄여감(h)
+            return l-i
+    return 0
